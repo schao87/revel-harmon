4358e3dfe24d6ecd26fc3a33057fc5f28758e7cb
schao87 <schaosti@gmail.com>
2014-03-20 23:27:48 -0400

    worked on js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 // JavaScript Document
 $(document).ready(function(e) {
     console.log($(document).height(),$(window).height());
+// we need window height and set the variables
+var docHeight= $(document).height();
+var winHeight= $(window).height();
+// make if statement to tell when browser scrolls to certain point
+$( window ).scroll(function() {
+  console.log("hello");
+  if ($(window).scrollTop()>37){console.log("scrolling")};
+});
+//if ($(window).scrollTop()>10){console.log("scrolling")};
+//make another class that will replace the current class 
+// make current class what it should look like. make another class with fixed position and -460px. then add it on in the beginning.
+// set div to correct position by adding and removing classes
+
 });
