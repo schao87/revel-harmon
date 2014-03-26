3253513f581bbf32f174bb1cabca91e9fedc15d5
schao87 <schaosti@gmail.com>
2014-03-25 20:14:47 -0400

    made page 2 work

--- js/script.js
+++ js/script.js
@@ -5,13 +5,19 @@ $(document).ready(function(e) {
 var docHeight= $(document).height();
 var winHeight= $(window).height();
 // make if statement to tell when browser scrolls to certain point
-$( window ).scroll(function() {
-  console.log("hello");
-  if ($(window).scrollTop()>37){console.log("scrolling")};
+$( window ).scroll(function() 
+{
+  	console.log("hello");
+  	if ($(window).scrollTop()>37)
+	{
+		//console.log("scrolling");
+		$(".page2Cont .scrollDown").css('visibility', 'visible');
+       $(".fauxScroll").hide();
+	};
 });
 //if ($(window).scrollTop()>10){console.log("scrolling")};
 //make another class that will replace the current class 
 // make current class what it should look like. make another class with fixed position and -460px. then add it on in the beginning.
 // set div to correct position by adding and removing classes
 
-});
+});	
